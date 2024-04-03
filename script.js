b664bfc12ce7ae25c6bc56920d60d297d2c7c0a6
Akinseye Benjamin <benjaminola222@gmail.com>
2024-04-03 13:29:12 +0100

    final

--- script.js
+++ script.js
@@ -11,17 +11,44 @@ let scrollContainer = document.querySelector(".gallery")
 let backBtn = document.getElementById("backBtn")
 let nextBtn = document.getElementById("nextBtn")
 
-scrollContainer.addEventListener("wheel", (evt) =>{
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-    scrollContainer.style.scrollBehavior = "auto"
-});
 
-backBtn.addEventListener("click", ()=>{
+function back(){
     scrollContainer.style.scrollBehavior = "smooth"
     scrollContainer.scrollLeft += 900;
-});
-nextBtn.addEventListener("click", ()=>{
+}
+
+function next(){
     scrollContainer.style.scrollBehavior = "smooth"
     scrollContainer.scrollLeft -= 900;
-});
+}
+
+
+let minus = document.querySelector('.minus')
+let plus = document.querySelector('.plus')
+let p3 = document.querySelector('.p3')
+let priceTotal = document.getElementById("priceTotal")
+
+plus.addEventListener('click', increase)
+
+function increase(){
+    let p3 = document.querySelector('.p3').innerHTML;
+    p3++;
+    console.log(p3);
+    document.querySelector('.p3').innerHTML = p3++
+    let priceTotal = document.getElementById("priceTotal").innerHTML
+    parseInt(priceTotal) + 5000;
+    document.getElementById("priceTotal").innerHTML = parseInt(priceTotal) + 5000;   
+    
+}
+
+minus.addEventListener('click', decrease)
+function decrease(){
+    if(document.querySelector('.p3').innerHTML >= 2){
+    let p3 = document.querySelector('.p3').innerHTML;
+    p3--;
+    document.querySelector('.p3').innerHTML = p3--
+    let priceTotal = document.getElementById("priceTotal").innerHTML
+    parseInt(priceTotal) - 5000;
+    document.getElementById("priceTotal").innerHTML = parseInt(priceTotal) - 5000; 
+}
+}
